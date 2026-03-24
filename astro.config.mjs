59b9be602f2608c47bd0e9a32d044e97ffee1430
rps <richparrishx@gmail.com>
2026-03-24 16:58:46 -0400

    add resend beta access api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: cloudflare(),
   site: "https://richparrish.dev",
   vite: {
     plugins: [tailwindcss()],
